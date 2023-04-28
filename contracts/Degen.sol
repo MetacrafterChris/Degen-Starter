122b5a0fb1e3e79311b231270f24726537b7cdb0
MetacrafterChris <125202934+MetacrafterChris@users.noreply.github.com>
2023-04-28 22:19:36 +0800

    Update Degen.sol

--- contracts/Degen.sol
+++ contracts/Degen.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Degen is ERC20, Ownable {
+contract DegenToken is ERC20, Ownable {
     constructor() ERC20("Degen", "DGN") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
